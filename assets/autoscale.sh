8432ce07e9cb21d3ad7152fc1af770a3d69ecdfc
lazulyra <40833165+lazulyra@users.noreply.github.com>
2025-02-16 00:29:27 +0100

    add textures

---
+++ assets/autoscale.sh
@@ -0,0 +1,3 @@
+for imgfile in ./1x/*.png; do
+    ffmpeg -i $imgfile -vf "scale=iw*2:ih*2" -sws_flags neighbor -y "${imgfile/1x/2x}"
+done
---
+++ main.lua
@@ -0,0 +1,3 @@
+SMODS.Atlas { key = "8BitDeck_opt2", path = "8BitDeck_opt2-celeste.png", px = 71, py = 95, raw_key = true }
+SMODS.Atlas { key = "Joker", path = "Jokers-celeste.png", px = 71, py = 95, raw_key = true }
+SMODS.Atlas { key = "Tarots", path = "Tarots-celeste.png", px = 34, py = 34, raw_key = true, atlas_table = 'ANIMATION_ATLAS', frames = 21 }
