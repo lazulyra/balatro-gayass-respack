64b6ca9b417bf8d00ca20297ba182c913f73ba1d
lazulyra <40833165+lazulyra@users.noreply.github.com>
2025-02-22 22:07:14 +0100

    music framework

--- main.lua
+++ main.lua
@@ -8,10 +8,11 @@ SMODS.Language { key = "en-us", label = "English", font = {
     DESCSCALE = 1
 }, loc_key = "en-us" }
 
-SMODS.Atlas { key = "cards_2", path = "8BitDeck_opt2-gayass_respack.png", px = 71, py = 95, prefix_config = { key = false } }
-SMODS.Atlas { key = "Joker", path = "Jokers-gayass_respack.png", px = 71, py = 95, prefix_config = { key = false } }
-SMODS.Atlas { key = "Tarot", path = "Consumable-gayass_respack.png", px = 71, py = 95, prefix_config = { key = false } }
-SMODS.Atlas { key = "Spectral", path = "Consumable-gayass_respack.png", px = 71, py = 95, prefix_config = { key = false } }
-SMODS.Atlas { key = "Planet", path = "Consumable-gayass_respack.png", px = 71, py = 95, prefix_config = { key = false } }
-SMODS.Atlas { key = "Centers", path = "Enhancers-gayass_respack.png", px = 71, py = 95, prefix_config = { key = false } }
-SMODS.Atlas { key = "Voucher", path = "Vouchers-gayass_respack.png", px = 71, py = 95, prefix_config = { key = false } }
+SMODS.Atlas { key = "cards_2", path = "8BitDeck_opt2-gayass_respack.png", px = 71, py = 95, prefix_config = false }
+SMODS.Atlas { key = "Joker", path = "Jokers-gayass_respack.png", px = 71, py = 95, prefix_config = false }
+SMODS.Atlas { key = "Tarot", path = "Consumable-gayass_respack.png", px = 71, py = 95, prefix_config = false }
+SMODS.Atlas { key = "Spectral", path = "Consumable-gayass_respack.png", px = 71, py = 95, prefix_config = false }
+SMODS.Atlas { key = "Planet", path = "Consumable-gayass_respack.png", px = 71, py = 95, prefix_config = false }
+SMODS.Atlas { key = "Centers", path = "Enhancers-gayass_respack.png", px = 71, py = 95, prefix_config = false }
+SMODS.Atlas { key = "Voucher", path = "Vouchers-gayass_respack.png", px = 71, py = 95, prefix_config = false }
+SMODS.Sound { key = "music1", path = "music1.ogg", prefix_config = false }
