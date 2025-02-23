52b55c62a3e391540ce833885b42f12cd1670f9e
lazulyra <40833165+lazulyra@users.noreply.github.com>
2025-02-23 12:47:16 +0100

    sfx framework

--- main.lua
+++ main.lua
@@ -8,11 +8,25 @@ SMODS.Language { key = "en-us", label = "English", font = {
     DESCSCALE = 1
 }, loc_key = "en-us" }
 
-SMODS.Atlas { key = "cards_2", path = "8BitDeck_opt2-gayass_respack.png", px = 71, py = 95, prefix_config = false }
+-- Atlases
+SMODS.Atlas { key = "cards_2", path = "8BitDeck_opt2-gayass_respack.png", px = 71, py = 95, prefix_config = false } -- cards_2: high contrast colours card set
 SMODS.Atlas { key = "Joker", path = "Jokers-gayass_respack.png", px = 71, py = 95, prefix_config = false }
 SMODS.Atlas { key = "Tarot", path = "Consumable-gayass_respack.png", px = 71, py = 95, prefix_config = false }
 SMODS.Atlas { key = "Spectral", path = "Consumable-gayass_respack.png", px = 71, py = 95, prefix_config = false }
 SMODS.Atlas { key = "Planet", path = "Consumable-gayass_respack.png", px = 71, py = 95, prefix_config = false }
-SMODS.Atlas { key = "Centers", path = "Enhancers-gayass_respack.png", px = 71, py = 95, prefix_config = false }
+SMODS.Atlas { key = "Centers", path = "Enhancers-gayass_respack.png", px = 71, py = 95, prefix_config = false } -- Centers: enhancers. not sure why it doesn't work for the Soul, will have to work out later
 SMODS.Atlas { key = "Voucher", path = "Vouchers-gayass_respack.png", px = 71, py = 95, prefix_config = false }
+
+-- Music
+--[[
+music1: main theme
+music2: booster theme (not including Planet)
+music3: planet booster theme
+music4: shop music
+music5: boss music
+--]]
 SMODS.Sound { key = "music1", path = "music1.ogg", prefix_config = false }
+
+-- Sounds
+SMODS.Sound { key = "chips1", path = "chips1.ogg", prefix_config = false }
+
